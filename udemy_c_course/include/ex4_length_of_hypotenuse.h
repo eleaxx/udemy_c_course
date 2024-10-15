bef47c9db554c411d2b78d818864b72ca5e50528
Elena Andrés <eiandres@indra.es>
2024-10-16 00:13:21 +0200

    Exercise 30

---
+++ udemy_c_course/include/ex4_length_of_hypotenuse.h
@@ -0,0 +1,6 @@
+#ifndef EX4_LENGTH_OF_HYPOTENUSE_H
+#define EX4_LENGTH_OF_HYPOTENUSE_H
+
+void calculate_hypotenuse();
+
+#endif
--- udemy_c_course/src/menu.c
+++ udemy_c_course/src/menu.c
@@ -3,6 +3,7 @@
 #include "../include/ex1_pay_raise.h"
 #include "../include/ex2_time_converter.h"
 #include "../include/ex3_trapezium_area.h"
+#include "../include/ex4_length_of_hypotenuse.h"
 
 void run_menu() {
 
@@ -14,6 +15,7 @@ void run_menu() {
                "\t 1 -> Calculate a pay raise.\n"
                "\t 2 -> Convert years into other time units.\n"
                "\t 3 -> Calculate the area of a trapezium.\n"
+               "\t 4 -> Calculate the lenght of an hypotenuse.\n"
                "\t 0 -> Exit.\n");
         scanf("%i", &option);
         switch(option){
@@ -22,10 +24,13 @@ void run_menu() {
                 break;
             case 2:
                 timer_converter();
-            break;
+                break;
             case 3:
                 trapezium_area();
-            break;
+                break;
+            case 4:
+                calculate_hypotenuse();
+                break;
             case 0:
                 printf("SEE YOU SOON!\n");
                 break;
