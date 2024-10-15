9f1ada0bd354639c30db9cd93b67850df260f541
Elena Andrés <eiandres@indra.es>
2024-10-16 00:09:49 +0200

    Exercise 19

--- udemy_c_course/src/menu.c
+++ udemy_c_course/src/menu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "../include/menu.h"
 #include "../include/ex1_pay_raise.h"
+#include "../include/ex2_time_converter.h"
 
 void run_menu() {
 
@@ -10,12 +11,16 @@ void run_menu() {
     do{
         printf("\nOptions:\n"
                "\t 1 -> Calculate a pay raise.\n"
+               "\t 2 -> Convert years into other time units.\n"
                "\t 0 -> Exit.\n");
         scanf("%i", &option);
         switch(option){
             case 1:
                 pay_raise();
                 break;
+            case 2:
+                timer_converter();
+            break;
             case 0:
                 printf("SEE YOU SOON!\n");
                 break;
